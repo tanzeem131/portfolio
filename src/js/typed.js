bd03e4c2bdcc1387d33bca150dac91b64590284d
tanzeem131 <syedtanzeem131@gmail.com>
2024-03-01 20:19:43 +0530

    made some changes

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,5 +1,5 @@
 let typed = new Typed('.auto-type', {
-  strings: ['a frontend developer', 'a UI designer', 'a proud engineer'],
+  strings: ['a frontend developer', 'a UI designer', 'a skilled engineer'],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
